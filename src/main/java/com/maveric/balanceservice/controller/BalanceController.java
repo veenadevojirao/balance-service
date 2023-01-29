205e6d74169c80334df92abc44accbed862a8aee
Veena Devojirao <veenade@maveric-systems.com>
2023-01-29 10:39:57 +0530

    resolving conflicts

--- src/main/java/com/maveric/balanceservice/controller/BalanceController.java
+++ src/main/java/com/maveric/balanceservice/controller/BalanceController.java
@@ -1,15 +1,16 @@
 package com.maveric.balanceservice.controller;
 
-import com.maveric.balanceservice.dto.BalanceDto;
 import com.maveric.balanceservice.exception.AccountIdMismatchException;
 import com.maveric.balanceservice.exception.BalanceIDNotFoundException;
-import com.maveric.balanceservice.repository.BalanceRepository;
 import com.maveric.balanceservice.service.BalanceService;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
