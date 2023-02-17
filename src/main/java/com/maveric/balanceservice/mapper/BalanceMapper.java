14556e612f4108225fd7922d5983ad073d81f561
veenade <veenade@maveric-systems.com>
2023-02-17 11:49:55 +0530

    updating db connection

--- src/main/java/com/maveric/balanceservice/mapper/BalanceMapper.java
+++ src/main/java/com/maveric/balanceservice/mapper/BalanceMapper.java
@@ -2,9 +2,11 @@ package com.maveric.balanceservice.mapper;
 
 import com.maveric.balanceservice.dto.BalanceDto;
 import com.maveric.balanceservice.entity.Balance;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface BalanceMapper {
     Balance dtoToEntity(BalanceDto balanceDto);
 
