356c0916ff81201d20641fc43cfaedeb4006b332
veenade <veenade@maveric-systems.com>
2023-02-24 12:23:32 +0530

    updating the feign changes

--- src/main/java/com/maveric/balanceservice/feignclient/AccountFeignService.java
+++ src/main/java/com/maveric/balanceservice/feignclient/AccountFeignService.java
@@ -15,12 +15,12 @@ import java.util.List;
 
 @FeignClient(value = "account-service")
 public interface AccountFeignService {
-    @GetMapping("/customers/{customerId}/accounts")
+    @GetMapping("api/v1/customers/{customerId}/accounts")
     ResponseEntity<List<Account>> getAccountantId(@PathVariable String customerId);
-    @GetMapping("customers/{customerId}/accounts/{accountId}")
+    @GetMapping("api/v1/customers/{customerId}/accounts/{accountId}")
     public AccountDto getAccount(@PathVariable("customerId") String customerId,
                                  @Valid @PathVariable("accountId") String accountId,
-                                 @RequestHeader(value = "userid") String headerUserId) throws AccountNotFoundException, CustomerIDNotFoundExistsException;
+                                 @RequestHeader(value = "userid") String headerUserId);
 
 
 }
